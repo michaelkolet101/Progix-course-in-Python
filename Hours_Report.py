fbb1a3eea393af6df6bf97ae4c791c8168925432
michael <michael24kolet@gmail.com>
2023-01-04 11:26:13 +0200

    auotomation to hours report

--- Hours_Report.py
+++ Hours_Report.py
@@ -2,17 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
-from selenium.webdriver import ActionChains
+
 import time
+from datetime import date
 
 
 URL = "https://mpg.co.il/login/?next=/"
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", False)
+chrome_options.add_experimental_option("detach", True)
+today = date.today()
+
+# The date will always be the day the script is run
+my_day = today.strftime("%d/%m/%Y")
+
+# Change the hours according to your hours
+start = "18:30"
+end = "20:30"
+
+# need to enter your id here
+your_id = " "
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
@@ -20,35 +30,33 @@ driver.maximize_window()
 driver.get(URL)
 
 
+
 username = driver.find_element(By.ID, "id_username")
-username.send_keys("my_id")
+username.send_keys(your_id)
 
 password = driver.find_element(By.ID, "id_password")
-password.send_keys("my_id")
+password.send_keys(your_id)
 
 submit = driver.find_element(By.CLASS_NAME, "btn")
 submit.click()
 time.sleep(3)
 
-report_type = driver.find_element(By.ID, "report_type")
-report_type.click()
 
-select = Select(report_type)
+select = Select(driver.find_element(By.ID, "report_type"))
 select.select_by_value("4")
 
-time.sleep(1)
+set_day = driver.find_element(By.ID, "report_date")
+set_day.send_keys(my_day)
 
-# TODO understent why it not working!
+set_time = driver.find_element(By.ID, "time_start")
+set_time.send_keys(start)
 
-report_date = driver.find_element(By.ID, "report_date")
-ActionChains(report_date).click()
-time.sleep(5)
+end_time = driver.find_element(By.ID, "time_finish")
+end_time.send_keys(end)
 
-# report_date.click()
+comments = driver.find_element(By.ID, "comments")
+comments.send_keys("שיעור בפייתון לקבוצה שלי")
 
-time_start = driver.find_element(By.ID, "time_start")
-
-time_finish = driver.find_element(By.ID, "time_finish")
-
-
+submit = driver.find_element(By.CLASS_NAME, "btn-info")
+submit.click()
 

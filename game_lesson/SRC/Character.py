b19f4efc1c20d808a58af21a035d6f5713f2f626
michaelkolet <michael24kolet@gmail.com>
2023-05-28 13:30:18 +0300

    28-05-23

---
+++ game_lesson/SRC/Character.py
@@ -0,0 +1,64 @@
+import pygame
+import random
+import time
+
+IMAGE_SIZE = (100, 100)
+
+class Slime(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.IMAGE_SIZE = (100, 100)
+        self.image = pygame.image.load(r"C:\Users\micha\Desktop\game_lesson\IMAGES\example.png").convert()
+        self.image = pygame.transform.scale(self.image, IMAGE_SIZE)
+        #self.image.set_colorkey('black')
+
+        self.rect = self.image.get_rect(center=(10,10))
+        
+
+
+
+        self.isJump = False
+        self.jumpCount = 10 
+
+    def get_pos(self):
+        return self.rect.x, self.rect.y
+    
+    
+    def move(self, new_x, new_y):
+        self.rect.x += new_x
+        self.rect.y += new_y
+
+        if self.rect.x > 500 or self.rect.y > 500:
+            self.rect.x = random.randint(1,499)
+            self.rect.y = 0
+        
+
+    def jump(self):
+        # Check if mario is jumping and then execute the
+        # jumping code.
+        if self.isJump:
+            if self.jumpCount >= -10:
+                neg = 1
+                if self.jumpCount < 0:
+                    neg = -1
+                self.rect.y -= self.jumpCount**2 * 0.3 * neg
+                self.jumpCount -= 1
+            else:
+                self.isJump = False
+                self.jumpCount = 10
+        self.center()
+
+
+    def center(self):
+        if self.rect.y < -100 or self.rect.y > 600:
+            self.rect.y = 0
+            self.rect.x = random.randint(1, 499)
+        if self.rect.x > 600 or self.rect.x < -100:
+            self.rect.x = 250
+
+    def diraction(self):
+        return pygame.transform.flip(self.image, True, False)
+    
+    def get_image_size(self):
+        return self.IMAGE_SIZE
--- game_lesson/SRC/main.py
+++ game_lesson/SRC/main.py
@@ -1,9 +1,13 @@
 import pygame
+import random
+from Character import Slime
+import pygame.sprite
 
 pygame.init()
 
 clock = pygame.time.Clock()
-REFRESH_RATE = 60
+
+REFRESH_RATE = 15
 
 screen = pygame.display.set_mode( (500, 500) )
 
@@ -12,66 +16,49 @@ pygame.display.set_caption("MY NEW GAME!!!")
 
 backgruond = pygame.image.load(r"C:\Users\micha\Desktop\game_lesson\IMAGES\bg.jpg").convert()
 
-# שינוי גודל התמונה שיהיה כמו הגודל של מסך המשחק
+# # שינוי גודל התמונה שיהיה כמו הגודל של מסך המשחק
 backgruond = pygame.transform.scale(backgruond, (500, 500))
 
-# הדמות במשחק
-bird = pygame.image.load(r"C:\Users\micha\Desktop\game_lesson\IMAGES\example.png").convert()
-bird = pygame.transform.scale(bird, (100, 100))
-bird.set_colorkey('black')
+all_sprites_list = pygame.sprite.Group()
+
+c1 = Slime()
+c2 = Slime()
+
+c1.image = c1.diraction()
+
+c1.rect.x = 250
+c1.rect.y = 250
+
+all_sprites_list.add(c1)
+all_sprites_list.add(c2)
 
 finish = False
 
-circle_x = 0
-circle_y = 250
 
 while finish == False:
 
-    screen.blit(backgruond, (0,0))
-    #screen.fill('red')
-    
-    screen.blit(bird, (circle_x, circle_y))
-    screen.blit(bird, (circle_x + 50, circle_y + 50))
-    screen.blit(bird, (circle_x + 100, circle_y + 100))
-    #pygame.draw.circle(screen, 'white', [circle_x, circle_y], 50, 0)
-
-    
-    if circle_x > 500:
-        circle_x = -100
-
-    if circle_x < -100:
-        circle_x = 500
-    
-        
-
-    # רשימת האירועים שיכולים לקרות
     for event in pygame.event.get():
-
-        # לחצו על יציאה מהמשחק ולכן המשחק יוצא
         if event.type == pygame.QUIT:
             finish = True
-
-        # לעשות פקודה אם לחצנו מקש שהוא חץ ימינה אז הדמות יזוז ימינה
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                circle_x += 20
-                
-                
-            if event.key == pygame.K_LEFT:
-                circle_x -= 20
-                
-
             if event.key == pygame.K_UP:
-                circle_y -= 20
+                c1.isJump = True
+            if event.key == pygame.K_RIGHT:
+                c1.move(5, 0)
+            if event.key == pygame.K_LEFT:
+                c1.move(-5, 0)
+        
                 
-
-            if event.key == pygame.K_DOWN:
-                circle_y += 20
-                
-            pygame.key.set_repeat(100)
-
-            
-
+    screen.blit(backgruond, (0,0))
+    
+    
+    if pygame.sprite.collide_rect(c1, c2) == True:
+        print("hello")
+    
+    c2.move(0,12)
+    c1.jump()
+    pygame.key.set_repeat(30) 
+    all_sprites_list.draw(screen)  
     clock.tick(REFRESH_RATE)
     
     pygame.display.update()
